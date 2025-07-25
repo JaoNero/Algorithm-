47e8455bd2290e16d1a301b1d533a776b5912ae8
“JaoNero” <aom978436@gmail.com>
2025-07-25 15:16:55 +0700

    Add Scanner

--- main.java
+++ main.java
@@ -1,8 +1,27 @@
-public class main{
-    public static void main(String [] arg){
-        int num1 = 4;
-        int num2 = 6;
-        int sum = num1 + num2;
-        System.out.println("SUM of " + num1 +" and " + num2 +" is " + sum);//
+// public class main{
+//     public static void main(String [] arg){
+//         int num1 = 4;
+//         int num2 = 6;
+//         int sum = num1 + num2;
+//         System.out.println("SUM of " + num1 +" and " + num2 +" is " + sum);//
+//     }
+// }
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter num2: ");
+        int num2 = input.nextInt();
+
+        int sum = num1+num2;
+
+        System.out.println("Sum = " + sum);
+
     }
 }
